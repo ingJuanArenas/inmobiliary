f702844e664c4b0ac7a65b023077f6eb24621530
ingJuanArenas <juanarenasing@gmail.com>
2025-12-29 09:53:55 -0500

    fixing bugs and documentation

--- src/main/java/com/houses/inmobiliary/domain/dto/UpdateDTO.java
+++ src/main/java/com/houses/inmobiliary/domain/dto/UpdateDTO.java
@@ -1,9 +1,19 @@
 package com.houses.inmobiliary.domain.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record UpdateDTO(
+ @NotBlank
     String city,
+    @NotNull
     Double price,
+    @Min(value = 1)
+    @NotNull
     Integer bedrooms,
+    @Min(value = 1)
+    @NotNull
     Integer bathrooms
 ) {
     
